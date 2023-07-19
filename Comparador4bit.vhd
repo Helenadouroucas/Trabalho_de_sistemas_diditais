b37ce3c94c547c1cb155ea2cc899da52cb506481
Helenadouroucas <96700753+Helenadouroucas@users.noreply.github.com>
2023-07-19 01:36:05 -0300

    fix Comparador4bit.vhd

--- Comparador4bit.vhd
+++ Comparador4bit.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+USE ieee.std_logic_1164.all;
+
 
 entity Comparador4bit is
     Port ( in1 : in  STD_LOGIC_VECTOR(3 DOWNTO 0);           
@@ -12,6 +13,7 @@ architecture behave of Comparador4bit is
 
  SIGNAL maior: STD_LOGIC_VECTOR (3 downto 0);
  SIGNAL igual: STD_LOGIC_VECTOR (3 downto 0);
+signal nin1,nin2:std_logic;
 
 component Comparador1bit is
     Port ( in1 : in  STD_LOGIC;
@@ -20,14 +22,16 @@ component Comparador1bit is
 	   igualin : in  STD_LOGIC;
            maior : out  STD_LOGIC;
 	   igual : out STD_LOGIC);
-  end component;
+  end component Comparador1bit;
 
 begin
+nin1<=not in1(3);
+nin2<=not in2(3);
 
 Comparador1bit_0: Comparador1bit
     port map (
-        in1 => not in1(0),
-        in2 => not in2(0),    
+        in1 => nin1,
+        in2 => nin2,    
         maiorin => '0',
         igualin =>  '0',
         maior => maior(0),
@@ -36,8 +40,8 @@ Comparador1bit_0: Comparador1bit
 
 Comparador1bit_1: Comparador1bit
     port map (
-        in1 => in1(1),
-        in2 => in2(1),    
+        in1 => in1(2),
+        in2 => in2(2),    
         maiorin => maior(0),
         igualin =>  igual(0),
         maior => maior(1),
@@ -46,8 +50,8 @@ Comparador1bit_1: Comparador1bit
 
 Comparador1bit_2 : Comparador1bit
     port map (
-        in1 => in1(2),
-        in2 => in2(2),    
+        in1 => in1(1),
+        in2 => in2(1),    
         maiorin => maior(1),
         igualin =>  igual(1),
         maior => maior(2),
@@ -56,8 +60,8 @@ Comparador1bit_2 : Comparador1bit
 
 Comparador1bit_3: Comparador1bit
     port map (
-        in1 => in1(3),
-        in2 => in2(3),    
+        in1 => in1(0),
+        in2 => in2(0),    
         maiorin => maior(2),
         igualin =>  igual(2),
         maior => maior(3),
