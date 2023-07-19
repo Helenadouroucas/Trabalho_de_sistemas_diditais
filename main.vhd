4ecbf617658869bb1f878b8514bdd75a3a36c739
Helenadouroucas <96700753+Helenadouroucas@users.noreply.github.com>
2023-07-19 01:52:11 -0300

    Update main.vhd

--- main.vhd
+++ main.vhd
@@ -4,7 +4,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity main is
     Port ( clock1 : in  STD_LOGIC;           
  	   clock2: in  STD_LOGIC;
-           sel : in  STD_LOGIC_VECTOR (2 downto 0));
+           sel : in  STD_LOGIC_VECTOR (2 downto 0);
+	   num1: out std_logic_vector(3 downto 0);
+	   num2: out std_logic_vector(3 downto 0);
+	   overflow : out  STD_LOGIC;
+           zero : out  STD_LOGIC;
+           negativo : out  STD_LOGIC;
+	   carry : out STD_LOGIC;
+	   resultado : out  STD_LOGIC_vector(3 downto 0)
+);
 end main;
 
 architecture behave of main is
@@ -20,12 +28,12 @@ component Ula is
            zero : out  STD_LOGIC;
            negativo : out  STD_LOGIC;
 	   carry : out STD_LOGIC;
-	   resultado : out  STD_LOGIC);
+	   resultado : out  STD_LOGIC_vector(3 downto 0));
   end component Ula;
 
 component contador_vhdl is
     Port ( clock : in STD_LOGIC;
-	   in : out STD_LOGIC_VECTOR(3 downto 0));
+	   S : out STD_LOGIC_VECTOR(3 downto 0));
   end component contador_vhdl;
 
 begin
@@ -33,13 +41,13 @@ begin
 contador_1: contador_vhdl
     port map (
            clock => clock1,
-           in => in1
+           S => in1
 );
 
 contador_2: contador_vhdl
     port map (
            clock => clock2,
-           in => in2
+           S=> in2
 );
 
 
@@ -55,5 +63,7 @@ Ula_IST: Ula
 	   carry => carry,
 	   resultado => resultado
 );
+num1<=in1;
+num2<=in2;
 
 end behave;
